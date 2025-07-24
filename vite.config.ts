9a099ab4c1df14e29c78794af1d31d64a81a9587
Vinicius Meirelles <50549048+vmeirelle@users.noreply.github.com>
2025-07-24 21:59:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/kanjiquiz/', // ✅ REQUIRED for GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
